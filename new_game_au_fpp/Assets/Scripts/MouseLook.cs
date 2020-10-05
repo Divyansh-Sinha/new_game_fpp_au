b5356a5644aa4308c1d5f56a253c9c2c0b0447fc
Pervez Shoaib <50441829+PervezSH@users.noreply.github.com>
2020-10-05 12:23:41 +0530

    added_movement

---
+++ new_game_au_fpp/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    [SerializeField] Transform camera_transfrom;
+    public float mouse_sensitivity = 100f;
+
+    float yRotation = 0.0f;
+
+    [Header("For Camera Ease")]
+    [SerializeField] Transform eye_transform;
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    private void Update()
+    {
+        mouse_look();
+        camera_ease();
+    }
+    void mouse_look()
+    {
+        float Mousex = Input.GetAxis("Mouse X") * mouse_sensitivity * Time.deltaTime;
+        float Mousey = Input.GetAxis("Mouse Y") * mouse_sensitivity * Time.deltaTime;
+
+        yRotation -= Mousey;
+        yRotation = Mathf.Clamp(yRotation, -80f, 75f);
+
+        camera_transfrom.localRotation = Quaternion.Euler(yRotation, 0f, 0f);
+        this.gameObject.transform.Rotate(Vector3.up * Mousex);
+    }
+    void camera_ease()
+    {
+        camera_transfrom.position = Vector3.Lerp(camera_transfrom.position, eye_transform.position, 1.0f);
+    }
+}
---
+++ new_game_au_fpp/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] CharacterController controller;
+    [SerializeField] Animator player_anim;
+    [SerializeField] float character_mov_speeed_fwd = 20f;
+    [SerializeField] float character_mov_speed_sidew = 5f;
+
+    private void Update()
+    {
+        movement();
+        apply_animation();
+    }
+    void movement()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x * character_mov_speed_sidew + transform.forward * z * character_mov_speeed_fwd;
+
+        controller.Move(move * Time.deltaTime);
+    }
+    void apply_animation()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        player_anim.SetFloat("Input X", inputX);
+        player_anim.SetFloat("Input Y", inputY);
+    }
+}
