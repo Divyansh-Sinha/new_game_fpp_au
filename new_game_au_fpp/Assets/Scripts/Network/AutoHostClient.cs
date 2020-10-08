cca3f409a27dcc62589ca45d4a8eaa588ea646b0
Pervez Shoaib <50441829+PervezSH@users.noreply.github.com>
2020-10-08 20:04:13 +0530

    added local host/join

---
+++ new_game_au_fpp/Assets/Scripts/Network/AutoHostClient.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using Mirror;
+
+public class AutoHostClient : MonoBehaviour
+{
+    [SerializeField] NetworkManager networkManager;
+
+    public void join_local()
+    {
+        networkManager.networkAddress = "localhost";
+        networkManager.StartClient();
+    }
+}
