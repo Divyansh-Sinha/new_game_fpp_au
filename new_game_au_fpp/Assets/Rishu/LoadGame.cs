3fd109b4e3aceb0e6813648b42e3d8a728ea7f67
Divyansh-Sinha <61075271+Divyansh-Sinha@users.noreply.github.com>
2020-10-09 11:58:16 +0530

    Just Trying..

---
+++ new_game_au_fpp/Assets/Rishu/LoadGame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class LoadGame : MonoBehaviour
+{
+    public void OnMouseDown()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
